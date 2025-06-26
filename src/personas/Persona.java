61bdcda7065a1ad5d3ed288a2c6ce83da2b496fb
Diego Garcia Mendoza <diegosg826@gmail.com>
2025-06-26 03:32:08 -0600

    Agregado interfaces Calificable y Pagable, y clase Persona
    Agregado interfaces Calificable y Pagable, y clase Persona

---
+++ src/personas/Persona.java
@@ -0,0 +1,33 @@
+package src.personas;
+
+interface Pagable {
+    double calcularPago();
+}
+
+interface Calificable {
+    double calcularPromedio();
+}
+
+// ========================
+// CLASE ABSTRACTA PERSONA
+// ========================
+
+public abstract class Persona {
+    protected String nombre;
+    protected String id;
+
+    public Persona(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public abstract void mostrarInformacion();
+}
